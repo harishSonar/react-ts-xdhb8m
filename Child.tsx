c101f1de65394c01f299175a8735a2f713b24a6f
harishSonar <tech.harish.sonar@gmail.com>
2023-04-08 00:46:54 +0530

    1 file modified

--- Child.tsx
+++ Child.tsx
@@ -2,8 +2,12 @@ import * as React from 'react';
 import './style.css';
 
 function Child({ prevValue, currValue }) {
+  const changeColor = () => {
+    document.getElementById('childContainer').style.color = 'red';
+  };
+
   return (
-    <div>
+    <div id="childContainer">
       <h3> {`Previous Value ---  ${prevValue}`} </h3>
       <h3> {`Current Value---  ${currValue}`} </h3>
     </div>
