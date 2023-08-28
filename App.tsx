4015edd3e669e28f1fa5777602248993fe9fb874
harishSonar <tech.harish.sonar@gmail.com>
2023-08-28 14:48:55 +0530

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,24 +1,18 @@
 import * as React from 'react';
 import './style.css';
 import Child from './Child';
-// import { useCustomState } from './useCustomState.hooks';
+import { useCustomState } from './useCustomState.hooks';
 
 export default function App() {
-  let currentValue = '';
-  const setCurrentValue = (value) => {
-    currentValue = value;
-  };
-
   // Currently hardcoded, To be removed after implementation
   const prevValue = 'To display old Value';
   const currValue = 'To display current Value';
 
   return (
     <div>
-      <input onChange={(e) => setCurrentValue(e.target.value)} />
+      <input onChange={(e) => {}} />
       <button onClick={() => {}}>Change Value</button>
       <Child prevValue={prevValue} currValue={currValue} />
-      <button onClick={() => {}}>Change Color</button>
     </div>
   );
 }
